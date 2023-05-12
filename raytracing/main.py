82a76037db7a74d41abf6eb7efad8191f01a41d3
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-05-12 14:11:51 +0200

    masking

--- raytracing/main.py
+++ raytracing/main.py
@@ -18,6 +18,8 @@ colors_list = [
   (0, 0, 255)
 ]
 
+SHAPE = (1000, 1000, 3)
+
 def save_bitmap(bitmap: np.array, output_filename: str) -> None:
   im = Image.fromarray(bitmap)
   im.save("")
@@ -52,12 +54,31 @@ def raytrace(bitmap: np.array, scene: list[Sphere], lights: list[Light]):
       point = (col, row, 0)
       for s in scene:
         if s.overlaps(point):
-          # funkcja
           coeff = s.light_coeff((row, col), lights[0])
           bitmap[row, col] = tuple(v * coeff for v in s.color)
 
+def get_neighbor_index(point: tuple[int, int]) -> list[tuple[int,int]]:
+  cords = [(-1, -1), (0, -1), (1, -1), (-1, 0), (0, 0), (1, 0), (-1, 1), (0, 1), (1, 1)]
 
-def main() -> None:
+  for c in cords:
+    yield(tuple(np.add(point, c)))
+
+def mask(bitmap: np.array, shape=SHAPE) -> np.array:
+  new_bitmap = create_empty_bitmap(shape)
+  weights = np.array([-1, 0, 1, -1, 1, 1, -1, 0, 1])
+  for row in range(len(bitmap[1:-1])):
+    for col in range(len(bitmap[row][1:-1])):
+      point_neighbors = list(get_neighbor_index((row, col)))
+      new_pixels = []
+      for neighbor, weight in zip(point_neighbors, weights):
+        new_pixels.append(tuple([x * weight for x in bitmap[neighbor]]))
+
+      avg_tuple = tuple(sum(x) / len(new_pixels) for x in zip(*new_pixels))
+      new_bitmap[row, col] = avg_tuple
+
+  return new_bitmap
+
+def raytracing_plot():
   lights = [
     Light((800, 800, 500), colors.BLUE)
   ]
@@ -72,7 +93,16 @@ def main() -> None:
   size = (1000,1000, 3)
   bitmap = create_empty_bitmap(size)
   raytrace(bitmap, spheres, lights)
+  newb = mask(bitmap)
   plot_bitmap(bitmap)
+  plot_bitmap(newb)
+
+def main() -> None:
+  I = np.asarray(Image.open('./img/1200px-Norwegian_hen.jpg'))
+  print(I.shape)
+  b = mask(I, I.shape)
+  plot_bitmap(b)
+
 
 if __name__ == "__main__":
   main()
